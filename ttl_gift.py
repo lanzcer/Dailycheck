6b197bc905acd649b23aa0f076e10ad0303a889c
Seeyou66 <73346894+Seeyou66@users.noreply.github.com>
2022-02-14 20:44:39 +0800

    Update ttl_gift.py

--- ttl_gift.py
+++ ttl_gift.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 cron: 0 0/30 * * * ?
-new Env('test太太乐测试test');，
+new Env('太太乐话费库存监控,兑换');，
 如需自动兑换话费，需要token和userId
 填写方式如:aaaa-bbbb-cccc-dddd-eeee,111111
 多账号用&连接或者另加同名变量
