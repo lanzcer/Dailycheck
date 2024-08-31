7b59e38272b72e0a3d7c96d9ce87a8c9b915fcb7
lanzcer <634720220@qq.com>
2024-08-31 12:31:46 +0800

    Update youchu.py

--- youchu.py
+++ youchu.py
@@ -44,8 +44,8 @@ def get_token_userid():
 
 
 class TaskCommitter:
-    def __init__(self, posapp_token, activity_id):
-        self.posapp_token = posapp_token
+    def __init__(self, token, activity_id):
+        self.token = token
         self.activity_id = activity_id
         self.base_url = "https://m.ujia007.com/ujia_mall/api/"
         self.base_headers = {
@@ -55,9 +55,9 @@ class TaskCommitter:
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
             'Origin': 'https://m.ujia007.com',
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/20D67 NebulaSDK/1.8.100112 Nebula Source/YOUBankApp/psbc UnionPay/1.0 PSBC/newPsbc WK PSDType(1) mPaaSClient/(null)',
-            'Cookie': f'SESSION=ZTlhOWUxNmEtMzBkNi00YjU1LThjYjktZjIxNTFkZDY1OWJi; POSAPP_token={self.posapp_token}; client=ujia_mall; customer_key=POSAPP; isFirstLogin=0; last_login_env=psBcApp; miniOpenId=; openid=; sessionid=e9a9e16a-30d6-4b55-8cb9-f2151dd659bb; stationId=218; stationKey=psbcAppForUser; token={self.posapp_token}; umengId=1281192848; userid=UJIAUSER221213162784847; station_umengId=1281192848; CNZZDATA1281192848=90733685-1722471363-%7C1722911502; acw_tc=784e2c8d17229114994246519e255dd5ef3706464f384dea577e4d6f1f8127; tfstk=fpuSdpxadBOPWRH1Cud4cYwdvo4uVvTZp6NKsXQP9zU-RmGZUbRuJyBxcvH048FrYeaIDxOu8vzRpwMQnJRl4XCQ9xkxdkAuLJeILXnzN2TZZb43J29w7FW2eg-nFaUd2BFYO5krNbusZb4hidJw7FllALsVNQaKprUYZ54dJWHdkEe0tWB89adjM-VLJ7eRJtFYt5Wdeud_33NDvRcWMaA8ZROzVbef5_gbersoV-_dJj7aVRQUhwQKl7hDukwf7KqjAfuzcSLCOSisMv2-we1QGcGxhluV3gPGQRwEgMs3ewFfqIOfxMltLG9jERYtiuF0gXRXGMkz2SVbQIOfxMq8iSjyGIsEU; UM_distinctid=19085a2661215ed-06f24901cfa7958-5012524f-51bf4-19085a266132bff',
+            'Cookie': f'SESSION=YWFjYzY0ZmUtMjg5Yy00MTFlLTljNzQtMjVmOWYwZTA3Mzgx; POSAPP_token={self.token}; client=ujia_mall; customer_key=POSAPP; isFirstLogin=0; last_login_env=psBcApp; miniOpenId=; openid=; sessionid=aacc64fe-289c-411e-9c74-25f9f0e07381; stationId=218; stationKey=psbcAppForUser; token={self.token}; umengId=1281192848; userid=UJIAUSER221213162784847; station_umengId=1281192848; usertoken={self.token}; CNZZDATA1281192848=90733685-1722471363-%7C1724991375; acw_tc=784e2c9017249913506411518e1b9464ce7392b4142a6583c99c59a629b052; tfstk=fdFtQCToMpdqs1e0KP6nimv0ogGOZSUwRlzWimmMhkELxDvgoRXaRygSx5mgSfOfHlqAbAXamZELPuQqfGlgHKZuoC4isrVpDlqkixNmmSQx_W2MicjZMrZoHoIlZ_4a7blfqgA1NrdrGq1mcTWsYslqGgjhZ_4a7jroLk7kMy_KoqxslogXOw3-kjOshC9BADujGmZjGalw9mYxxIeXKhZwVyipGIUxWfi5aDdXGW39q0aOUIOb9VH_iKVWGQm7XrFZE59Ah2FTyS3UJTpSJ5U8MqFV06Iyguq8lw-o8XpOJS923Agn6f8BuVIEbiuKqwxM3KkOr2nlJwp23AgEJ0bUgKJqBaf..; UM_distinctid=19085a2661215ed-06f24901cfa7958-5012524f-51bf4-19085a266132bff',
             'Host': 'm.ujia007.com',
-            'Referer': f'https://m.ujia007.com/ujia_mall/web/posActivity/tree/activity.html?activityId={self.activity_id}&customer_key=POSAPP&stationId=8d54292991d5e368&stationKey=psbcAppForUser&returnUrlKey=posTrees&loginSource=userCenter&uUserId=080601a344f10f7cde08fa978d3f94106585f87e441011fc&uSign=6B9978FAB7C40D12F37464258D915DE3&uTimesTamp=786d86af1482c3365316d9209500040e&isFirstLogin=0&token=59afbf0978384559a6590d5f2ad36368',
+            'Referer': f'https://m.ujia007.com/ujia_mall/web/posActivity/tree/activity.html?activityId={self.activity_id}&customer_key=POSAPP&stationId=8d54292991d5e368&stationKey=psbcAppForUser&returnUrlKey=posTrees&loginSource=userCenter&uUserId=080601a344f10f7cde08fa978d3f94106585f87e441011fc&uSign=D615EA43F719E004C18460122FBA92BA&uTimesTamp=d4458a61359eadac762a750f23b2792f&isFirstLogin=0&token={self.token}',
             'Accept-Language': 'zh-CN,zh-Hans;q=0.9',
             'Accept': '*/*'
         }
@@ -65,7 +65,7 @@ class TaskCommitter:
     def commit_task(self, task_code):
         url = self.base_url + "task/commitTask"
         body = {
-            'token': self.posapp_token,
+            'token': self.token,
             'activityId': self.activity_id,
             'taskCode': task_code
         }
@@ -78,7 +78,7 @@ class TaskCommitter:
     def get_user_collect_task_list(self):
         url = self.base_url + "task/getUserCollectTaskList"
         body = {
-            'token': self.posapp_token,
+            'token': self.token,
             'activityId': self.activity_id
         }
         response = requests.post(url, headers=self.base_headers, data=body)
@@ -92,19 +92,19 @@ class TaskCommitter:
     def get_task_user_num(self):
         url = self.base_url + "task/getTaskUserNum"
         body = {
-            'token': self.posapp_token,
+            'token': self.token,
             'activityId': self.activity_id
         }
         response = requests.post(url, headers=self.base_headers, data=body)
         tree_id = response.json()['data']['treeId']
-        tree_num = response.json()['ret']
+        tree_num = response.json()['data']['treesNum']
         return tree_id, tree_num
 
 
     def one_button_watering(self,tree_id):
         url = self.base_url + "task/oneButtonWatering"
         body = {
-            'token': self.posapp_token,
+            'token': self.token,
             'activityId': self.activity_id,
             'treeId': tree_id
         }
